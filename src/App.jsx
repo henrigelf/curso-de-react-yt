d48139e77dabcb6c60f6351f0150b02b84d266b0
henrigelf <38732959+henrigelf@users.noreply.github.com>
2025-04-27 23:46:31 -0300

    comit inicial

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import Tasks from "./components/Tasks";
+import AddTasks from "./components/AddTasks";
+import { v4 } from "uuid";
+import Title from "./components/Title";
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks") || [])
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      // CHAMAR A API
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        { method: "GET" }
+      );
+
+      // PEGAR OS DADOS QUE ELA RETORNA
+      const data = await response.json();
+
+      // ARMAZENAR/PERSISTIR ESSES DADOS NO STATES
+      setTasks(data);
+    };
+    fetchTasks();
+  }, []);
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //PRECISA ATUALIZAR ESSA TAREFA
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      //NÃO PRECISO ATUALIZAR ESSA TAREFA
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskClick(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="W-[500px] space-y-4">
+        <Title>Gerenciador de tarefas 2</Title>
+        <AddTasks onAddTaskClick={onAddTaskClick} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
+}
+export default App;
